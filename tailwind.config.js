4e2912cfd189f9148dcf5da8a2d6753ac311c10d
Ahmed Safwat <ahmedbody866@gmail.com>
2022-07-14 21:57:48 -1200

    created the index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "20px",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
   },
   plugins: [],
-}
+};
