9602d0110d2e44bffdee0918934e3826e00c48c3
Ahmed Safwat <ahmedbody866@gmail.com>
2022-07-15 18:48:09 -1200

    added mobile navmenu

---
+++ assets/js/all.js
@@ -0,0 +1,21 @@
+// toggle header menu
+$(".menu-toggle").click(function () {
+  $(this).attr("aria-expanded", "true");
+  $(".aside-menu").removeClass("-translate-x-full");
+  $(".menu-overlay").fadeIn();
+  $(document.body).css("overflow", "hidden");
+});
+
+$(".aside-closer").click(function () {
+  $(".menu-toggle").attr("aria-expanded", "false");
+  $(".aside-menu").addClass("-translate-x-full");
+  $(".menu-overlay").fadeOut();
+  $(document.body).css("overflow", "visible");
+});
+
+$(".menu-overlay").click(function () {
+  $(".menu-toggle").attr("aria-expanded", "false");
+  $(".aside-menu").addClass("-translate-x-full");
+  $(this).fadeOut();
+  $(document.body).css("overflow", "visible");
+})
