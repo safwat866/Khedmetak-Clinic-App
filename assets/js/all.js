23b9d17f775e00da51243f44dc9e8d1d46edac67
Ahmed Safwat <ahmedbody866@gmail.com>
2022-07-19 20:40:37 -1200

    added more pages and optimized code

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,31 +1,49 @@
 // toggle header menu
-$(".menu-toggle").click(function () {
-  $(this).attr("aria-expanded", "true");
-  $(".aside-menu").removeClass("-translate-x-full");
-  $(".menu-overlay").fadeIn();
-  $(document.body).css("overflow", "hidden");
-});
-
-$(".aside-closer").click(function () {
+function hideMenu() {
   $(".menu-toggle").attr("aria-expanded", "false");
   $(".aside-menu").addClass("-translate-x-full");
   $(".menu-overlay").fadeOut();
   $(document.body).css("overflow", "visible");
+}
+
+function showMenu() {
+  $(this).attr("aria-expanded", "true");
+  $(".aside-menu").removeClass("-translate-x-full");
+  $(".menu-overlay").fadeIn();
+  $(document.body).css("overflow", "hidden");
+}
+
+$(".menu-toggle").click(function () {
+  showMenu();
+});
+
+$(".aside-closer").click(function () {
+  hideMenu();
 });
 
 $(".menu-overlay").click(function () {
-  $(".menu-toggle").attr("aria-expanded", "false");
-  $(".aside-menu").addClass("-translate-x-full");
-  $(this).fadeOut();
-  $(document.body).css("overflow", "visible");
+  hideMenu();
 });
 
 // fix body overflow in desktop
 $(window).resize(function () {
   if ($(this).width() > 992) {
-    $(".menu-toggle").attr("aria-expanded", "false");
-    $(".aside-menu").addClass("-translate-x-full");
-    $(".menu-overlay").fadeOut();
-    $(document.body).css("overflow", "visible");
+    hideMenu();
   }
+});
+
+// upload recipe image
+$(".upload-recipe").click(function () {
+  $(this).prev().click();
+});
+
+function showUploadedFile(file) {
+  const img = document.createElement("img");
+  img.src = URL.createObjectURL(file);
+  $(".img-box").append(img);
+}
+
+$(".recipe-input").change(function (e) {
+  const file = e.target.files[0];
+  showUploadedFile(file);
 });
